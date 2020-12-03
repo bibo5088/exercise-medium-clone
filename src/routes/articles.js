4f4fa95d736764287b29547d67806ae6f2822282
Corentin Crébier <crebier.corentin@gmail.com>
2020-12-03 20:18:32 +0100

    Add /api/articles/:id Route

--- src/routes/articles.js
+++ src/routes/articles.js
@@ -3,9 +3,9 @@ import knex from '../knex.js';
 const router = Router();
 
 router.get('/', async (req, res) => {
-  const articlesRaw = await knex('article AS a')
-    .join('user AS u', 'a.author_id', '=', 'u.id')
-    .join('category AS c', 'a.category_id', '=', 'c.id')
+  const articlesRaw = await knex({ a: 'article' })
+    .join({ u: 'user' }, 'a.author_id', '=', 'u.id')
+    .join({ c: 'category' }, 'a.category_id', '=', 'c.id')
     .select(
       'a.id',
       'a.name',
@@ -13,12 +13,12 @@ router.get('/', async (req, res) => {
       'a.thumbnail',
       'a.date',
 
-      'u.id AS user_id',
-      'u.name AS user_name',
-      'u.avatar AS user_avatar',
+      { user_id: 'u.id' },
+      { user_name: 'u.name' },
+      { user_avatar: 'u.avatar' },
 
-      'c.id AS category_id',
-      'c.name AS category_name'
+      { category_id: 'c.id' },
+      { category_name: 'c.name' }
     );
   const articles = articlesRaw.map((r) => ({
     id: r.id,
@@ -38,6 +38,104 @@ router.get('/', async (req, res) => {
   }));
 
   res.json(articles);
+});
+
+router.get('/:id(\\d+)', async (req, res) => {
+  const id = Number(req.params.id);
+
+  const rawArticle = await knex({ a: 'article' })
+    .leftJoin({ u: 'user' }, 'a.author_id', '=', 'u.id')
+    .leftJoin({ c: 'category' }, 'a.category_id', '=', 'c.id')
+    .leftJoin({ t: 'tag' }, (join) => {
+      join.onIn(
+        't.id',
+        knex('article_tag')
+          .select('article_tag.tag_id')
+          .where('article_tag.article_id', '=', knex.raw('a.id'))
+      );
+    })
+    .leftJoin({ co: 'comment' }, 'co.article_id', '=', 'a.id')
+    .leftJoin({ co_u: 'user' }, 'co_u.id', '=', 'co.user_id')
+    .where('a.id', '=', id)
+    .select(
+      'a.id',
+      'a.name',
+      'a.content',
+      'a.thumbnail',
+      'a.date',
+
+      { user_id: 'u.id' },
+      { user_name: 'u.name' },
+      { user_avatar: 'u.avatar' },
+
+      { category_id: 'c.id' },
+      { category_name: 'c.name' },
+
+      { tag_id: 't.id' },
+      { tag_name: 't.name' },
+
+      { comment_id: 'co.id' },
+      { comment_content: 'co.content' },
+      { comment_date: 'co.date' },
+
+      { comment_user_id: 'co_u.id' },
+      { comment_user_name: 'co_u.name' },
+      { comment_user_avatar: 'co_u.avatar' },
+
+      {
+        like_count: knex('liked').count('*').where('article_id', '=', id),
+      }
+    );
+
+  if (rawArticle == undefined) {
+    return res.status(404).end();
+  }
+
+  const article = {
+    id: rawArticle[0].id,
+    name: rawArticle[0].name,
+    content: rawArticle[0].content,
+    thumbnail: rawArticle[0].thumbnail,
+    date: rawArticle[0].date,
+    likeCount: rawArticle[0].like_count,
+    author: {
+      id: rawArticle[0].user_id,
+      name: rawArticle[0].user_name,
+      avatar: rawArticle[0].user_avatar,
+    },
+    category: {
+      id: rawArticle[0].category_id,
+      name: rawArticle[0].category_name,
+    },
+    tags: {},
+    comments: {},
+  };
+
+  for (const row of rawArticle) {
+    if (row.tag_id != null) {
+      article.tags[row.tag_id] = {
+        id: row.tag_id,
+        name: row.tag_name,
+      };
+    }
+
+    if (row.comment_id != null) {
+      article.comments[row.comment_id] = {
+        id: row.comment_id,
+        content: row.comment_content,
+        date: row.comment_date,
+        user: {
+          id: row.comment_user_id,
+          name: row.comment_user_name,
+          avatar: row.comment_user_avatar,
+        },
+      };
+    }
+  }
+  article.tags = Object.values(article.tags);
+  article.comments = Object.values(article.comments);
+
+  res.json(article);
 });
 
 export default router;
--- test/articles.integration.spec.js
+++ test/articles.integration.spec.js
@@ -5,20 +5,31 @@ import knex from '../src/knex.js';
 import {
   insertFakeArticle,
   insertFakeCategory,
+  insertFakeComment,
+  insertFakeTag,
   insertFakeUser,
 } from './utils.js';
 
+jest.setTimeout(30000);
 describe('/api/articles', () => {
   let user;
   let category1;
   let category2;
   let article1;
   let article2;
+  let comment1;
+  let comment2;
+  let tag1;
+  let tag2;
   beforeEach(async () => {
     await knex.raw('SET FOREIGN_KEY_CHECKS = 0');
     await knex('article').truncate();
     await knex('category').truncate();
     await knex('user').truncate();
+    await knex('comment').truncate();
+    await knex('article_tag').truncate();
+    await knex('tag').truncate();
+    await knex('liked').truncate();
     await knex.raw('SET FOREIGN_KEY_CHECKS = 1');
 
     user = await insertFakeUser();
@@ -28,6 +39,25 @@ describe('/api/articles', () => {
 
     article1 = await insertFakeArticle(user.id, category1.id);
     article2 = await insertFakeArticle(user.id, category2.id);
+
+    comment1 = await insertFakeComment(user.id, article1.id);
+    comment2 = await insertFakeComment(user.id, article1.id);
+    tag1 = await insertFakeTag();
+    tag2 = await insertFakeTag();
+
+    await knex('article_tag').insert({
+      tag_id: tag1.id,
+      article_id: article1.id,
+    });
+    await knex('article_tag').insert({
+      tag_id: tag2.id,
+      article_id: article1.id,
+    });
+
+    await knex('liked').insert({
+      user_id: user.id,
+      article_id: article1.id,
+    });
   });
 
   afterAll(() => {
@@ -75,5 +105,89 @@ describe('/api/articles', () => {
         },
       },
     ]);
+  });
+
+  it('GET /:id - Should return the matching article with tags and comments', async () => {
+    const res = await supertest(app)
+      .get(`/api/articles/${article1.id}`)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    expect(res.body).toEqual({
+      id: article1.id,
+      name: article1.name,
+      content: article1.content,
+      thumbnail: article1.thumbnail,
+      date: article1.date.toISOString(),
+      likeCount: 1,
+      author: {
+        id: user.id,
+        name: user.name,
+        avatar: user.avatar,
+      },
+      category: {
+        id: category1.id,
+        name: category1.name,
+      },
+      tags: [
+        {
+          id: tag1.id,
+          name: tag1.name,
+        },
+        {
+          id: tag2.id,
+          name: tag2.name,
+        },
+      ],
+      comments: [
+        {
+          id: comment1.id,
+          content: comment1.content,
+          date: comment1.date.toISOString(),
+          user: {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar,
+          },
+        },
+        {
+          id: comment2.id,
+          content: comment2.content,
+          date: comment2.date.toISOString(),
+          user: {
+            id: user.id,
+            name: user.name,
+            avatar: user.avatar,
+          },
+        },
+      ],
+    });
+  });
+
+  it('GET /:id - Should return the matching article with no tags and no comments', async () => {
+    const res = await supertest(app)
+      .get(`/api/articles/${article2.id}`)
+      .expect(200)
+      .expect('Content-Type', /json/);
+
+    expect(res.body).toEqual({
+      id: article2.id,
+      name: article2.name,
+      content: article2.content,
+      thumbnail: article2.thumbnail,
+      date: article2.date.toISOString(),
+      likeCount: 0,
+      author: {
+        id: user.id,
+        name: user.name,
+        avatar: user.avatar,
+      },
+      category: {
+        id: category2.id,
+        name: category2.name,
+      },
+      tags: [],
+      comments: [],
+    });
   });
 });
